bbb2d9cbeec9a54705ac7ad55bd1511aa9053f7f
Sarah Thomas <s4r4hthomas@gmail.com>
2019-10-27 20:52:47 -0400

    Added logic for winning conditions
    Last working version before it got fucked up lol

--- colorGame.js
+++ colorGame.js
@@ -1,7 +1,12 @@
 let color = ""; //initialize string to hold random RGB
+
 let winningValue = document.getElementById("winningValue");
-let colors = [getRandomRGB(), getRandomRGB(), getRandomRGB(), getRandomRGB(), getRandomRGB(), getRandomRGB()];
 let squares = document.querySelectorAll(".square");
+let body = document.querySelector("body");
+let messageDisplay = document.getElementById("message");
+let h1 = document.querySelector("h1");
+
+let colors = [getRandomRGB(), getRandomRGB(), getRandomRGB(), getRandomRGB(), getRandomRGB(), getRandomRGB()];
 
 //Randomizing the RGB values
 function getRandomRGB() {
@@ -17,21 +22,39 @@ function getRandomRGB() {
 function setSquares() {
     for (let i = 0; i < squares.length; i++) {
         squares[i].style.backgroundColor = colors[i];
-        squares[i].addEventListener("click", function(){ 
+        squares[i].addEventListener("click", function () {
             let clickedColor = this.style.backgroundColor;
-            if (clickedColor != winningValue.innerText.toLowerCase()){ //if clicked square is not the winning RGB
-                this.style.backgroundColor = document.querySelector("body").style.backgroundColor; //make the square appear to disappear (set it equal to body background color)
+            if (clickedColor === winningValue.innerText.toLowerCase()) { //if clicked square is not the winning RGB
+                winningConditions();
+            } else {
+                this.style.backgroundColor = body.style.backgroundColor; //make the square appear to disappear (set it equal to body background color)
+                messageDisplay.textContent = "Try Again"; //try again
             }
         });
     }
 }
 
 //Setting a winning color
-function setWinningValue() {
-    let winner = Math.floor(Math.random() * 6); //pick random number from 0 - 5 (coresponds to index of colors)
+function setWinningColor() {
+    let winner = Math.floor(Math.random() * colors.length); //pick random number from 0 - 5 (coresponds to index of colors)
     winningValue.textContent = colors[winner].toUpperCase(); //set the winning value to that color
+}
+
+//New Colors
+//add listening event for when clicked
+//if clicked, reset game (set squares & winning value)
+
+//Winning conditions
+function winningConditions() {
+    messageDisplay.textContent = "Correct!"; //correct!
+    for (let i = 0; i < squares.length; i++) { //set all squares to be the rgb value of the winning color
+        squares[i].style.backgroundColor = winningValue.innerText.toLowerCase();
+    }
+    h1.style.backgroundColor = winningValue.innerText.toLowerCase(); //set header to be the rgb value of the winning color
+    //ask user if they want to reset game
+    //new colors = Play again?
 }
 
 //Initializing the game
 setSquares();
-setWinningValue();
+setWinningColor();
